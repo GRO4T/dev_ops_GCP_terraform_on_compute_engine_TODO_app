19ce8e96e20c10aa8f7c7c8bda7d56c7a7008309
Damian Kolaska <damian.kolaska99@gmail.com>
2022-07-17 10:55:40 +0200

    Added CSS

---
+++ deploy_scripts/deploy.sh
@@ -0,0 +1,2 @@
+sudo cp *.php /var/www/html/
+sudo cp *.css /var/www/html/
--- index.php
+++ index.php
@@ -13,28 +13,38 @@ if (file_exists('todo.json')) {
     <meta charset="UTF-8">
     <meta http-equiv="Content-Type" content="text/html" />
     <title>TODO app</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <form action="new_todo.php" method="post">
-        <input type="text" name="todo_name" placeholder="Enter your todo">
-        <button>New TODO</button>
-    </form>
+    <fieldset>
+        <legend>TODO app</legend>
 
-<?php foreach ($todos as $todoName => $todo): ?>
-    <div style="margin-bottom: 5px;">
-        <form style="display: inline-block" action="change_status.php" method="post">
-            <input type="hidden" name="todo_name" value="<?php echo $todoName ?>">
-            <input type="checkbox"
-                   onChange="this.form.submit()"
-                   <?php echo $todo['completed'] ? 'checked' : '' ?>>
+        <form action="new_todo.php" method="post">
+            <input type="text" name="todo_name" placeholder="Enter your todo">
+            <button>New TODO</button>
         </form>
-        <?php echo $todoName ?>
-        <form style="display: inline-block" action="delete.php" method="post">
-            <input type="hidden" name="todo_name" value="<?php echo $todoName ?>">
-            <button>Delete</button>
-        </form>
-    </div>
-<?php endforeach ?>
 
+        <table>
+            <?php foreach ($todos as $todoName => $todo): ?>
+                    <tr>
+                        <td>
+                            <form action="change_status.php" method="post">
+                                <input type="hidden" name="todo_name" value="<?php echo $todoName ?>">
+                                <input type="checkbox"
+                                    onChange="this.form.submit()"
+                                    <?php echo $todo['completed'] ? 'checked' : '' ?>>
+                            </form>
+                            <?php echo $todoName ?>
+                        </td>
+                        <td>
+                            <form action="delete.php" method="post">
+                                <input type="hidden" name="todo_name" value="<?php echo $todoName ?>">
+                                <button>Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+            <?php endforeach ?>
+        </table>
+    </fieldset>
 </body>
 </html>
