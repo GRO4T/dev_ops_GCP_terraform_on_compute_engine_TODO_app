7840af55b39028241ede1d7339949d00920f60c0
Damian Kolaska <damian.kolaska99@gmail.com>
2022-08-15 14:15:40 +0200

    Deploy to a local VM automated using ansible

--- deploy_scripts/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo cp ./*.php /var/www/html/
-sudo cp ./*.css /var/www/html/
--- deploy_scripts/setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Install Apache web server and PHP
-sudo apt-get install apache2 php libapache2-mod-php
-sudo service apache2 start
