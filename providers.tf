fde6da02c1a1f483d375cfe81f85fad75ae00db0
Damian Kolaska <damian.kolaska99@gmail.com>
2022-08-21 21:03:20 +0200

    Terraform: add remote Cloud Storage backend

--- providers.tf
+++ providers.tf
@@ -2,3 +2,11 @@ provider "google" {
   project = "sandbox-351818"
   region  = "us-central1"
 }
+
+terraform {
+  backend "gcs" {
+    bucket = "sandbox-351818-tf-state
+"
+    prefix = "terraform/state"
+  }
+}
