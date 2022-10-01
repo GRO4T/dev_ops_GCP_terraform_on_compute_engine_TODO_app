f8c3b235c8a1449c0d4cf1fc7cbd2e1f7dd4cef0
Damian Kolaska <damian.kolaska99@gmail.com>
2022-10-01 12:30:12 +0200

    Add google provider version

--- providers.tf
+++ providers.tf
@@ -1,11 +1,18 @@
-provider "google" {
-  project = "sandbox-351818"
-  region  = "us-central1"
-}
-
 terraform {
   backend "gcs" {
     bucket = "sandbox-351818-tf-state"
     prefix = "terraform/state"
   }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.34.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "sandbox-351818"
+  region  = "us-central1"
 }
