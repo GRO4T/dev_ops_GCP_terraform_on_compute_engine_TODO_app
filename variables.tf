6a66b2600092877061fcc5ae77c4751b3c2f4a30
Damian Kolaska <damian.kolaska99@gmail.com>
2022-12-23 00:34:13 +0100

    fix: reformat variable.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "gcp_project_id" {
-    type = string
+  type = string
 }
 
 variable "gcp_region" {
-    type = string
-    default = "us-central1"
+  type    = string
+  default = "us-central1"
 }
 
 variable "gcp_zone" {
-    type = string
-    default = "us-central1-a"
+  type    = string
+  default = "us-central1-a"
 }
 
 variable "gce_instance_name" {
-    type = string
-    default = "todo-list-instance"
+  type    = string
+  default = "todo-list-instance"
 }
 
 variable "gce_machine_type" {
-    type = string
-    default = "e2-micro"
+  type    = string
+  default = "e2-micro"
 }
